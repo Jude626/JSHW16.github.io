157fee619a8a66d182f062ceeda6ef9ff367a04d
Jude <jantungkuno@gmail.com>
2021-03-28 22:01:40 -0400

    working on sitehead function to be imported

---
+++ employee-directory-hw-16/src/components/SiteHead/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SiteHead() {
+    return (
+        <div className = "wrapper">
+            <div className = "title">
+                Employee Directory
+            </div>
+        </div>
+        
+    )
+}

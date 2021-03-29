44ad33e6ca9832344fc69c153e2e1d4266f6a051
Jude <jantungkuno@gmail.com>
2021-03-28 22:28:16 -0400

    componentdidmount function for sorting

--- employee-directory-hw-16/src/components/ListHeader/index.js
+++ employee-directory-hw-16/src/components/ListHeader/index.js
@@ -9,5 +9,15 @@ class ListHeader extends React.Component {
         employees: []
     }
 
+    componentDidMount() {
+        if (this.state.sortedEmployees.length < 1) {
+            this.setState({
+                sortedEmployees: this.props.employeeList
+            })
+        }
+    }
+
+
+
     
 }

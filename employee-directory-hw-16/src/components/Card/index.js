dc74f9f050e3bbf1dc85b9f36b70834f9ee30cbe
Jude <jantungkuno@gmail.com>
2021-03-28 22:49:34 -0400

    finished props

--- employee-directory-hw-16/src/components/Card/index.js
+++ employee-directory-hw-16/src/components/Card/index.js
@@ -5,6 +5,10 @@ function Card(props) {
         <div className="card">
             <div><img alt={props.last} src={props.image}></img></div>
             <div>{props.title} {props.first} {props.last} </div>
+            <div>{props.gender}</div>
+            <div>{props.age}</div>
+            <div>{props.phone}</div>
+            <div>{props.email}</div>
         </div>
-    )
+    );
 }

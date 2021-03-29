f835d72908e76156309f2e53d6b5f1554a3c928e
Jude <jantungkuno@gmail.com>
2021-03-28 22:27:18 -0400

    added state for component

--- employee-directory-hw-16/src/components/ListHeader/index.js
+++ employee-directory-hw-16/src/components/ListHeader/index.js
@@ -2,5 +2,12 @@ import React from "react";
 import Card from "../Card";
 
 class ListHeader extends React.Component {
+    state = {
+        alphabetical: true,
+        ascending: true,
+        sortedEmployees: [],
+        employees: []
+    }
+
     
 }

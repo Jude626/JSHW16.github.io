2ab4edd80ea11920fbed6547e9822089e13c04fd
Jude <jantungkuno@gmail.com>
2021-03-28 21:31:27 -0400

    extend app with react.component, made a componentdidmount function

--- employee-directory-hw-16/src/App.js
+++ employee-directory-hw-16/src/App.js
@@ -1,2 +1,19 @@
 import React from 'react';
 import axios from 'axios';
+
+class App extends React.Component {
+    state = {
+      employees: [],
+    };
+
+    componentDidMount() {
+      axios.get(`https://randomuser.me/api/?results=20&nat=Aus`)
+      .then(res => {
+        this.setState({ employees: res.data.results });
+      });
+    }
+
+
+
+    
+}
